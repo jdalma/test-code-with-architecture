cf9bf7c06cfbb21f0060c994ae64823943e4d068
jeongdalma <jeongdalma@gmail.com>
2024-08-14 17:11:28 +0900

    PostService 테스트 코드 추가

---
+++ src/test/resources/sql/post-service-test-data.sql
@@ -0,0 +1,6 @@
+insert into `users` (`email`, `nickname`, `address`, `certification_code`, `status`, `last_login_at`)
+values ('jeongdalma@gmail.com', 'jeongdalma', 'Seoul', 'aaaaaaaa-aaaa-aaaa-aaaa-aaaaaaaaaaaa', 'ACTIVE', 0);
+insert into `users` (`email`, `nickname`, `address`, `certification_code`, `status`, `last_login_at`)
+values ('jeongdalma2@gmail.com', 'jeongdalma2', 'Seoul', 'aaaaaaaa-aaaa-aaaa-aaaa-aaaaaaaaaaab', 'PENDING', 0);
+insert into `posts` (`content`, `created_at`, `modified_at`, `user_id`)
+values ('helloworld', 1678530673958, 0, 1);
